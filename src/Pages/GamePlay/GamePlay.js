cf50dbac3dedeb6eef234d4bfcdde47ca5faa9f6
HENRY-KC <henrykc24@yahoo.com>
2021-04-02 18:12:02 +0100

    add game start page

--- src/Pages/GamePlay/GamePlay.js
+++ src/Pages/GamePlay/GamePlay.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import BackgroundImage from "../../Assets/gamePlayBg.jpg";
+import Styles from './Styles.module.css';
 
 const GamePlay = () => {
+  const {board} = Styles;
   return (
-    <div>
-      Game Play
-    </div>
-  )
-}
+    <div
+      style={{
+        backgroundImage: `url(${BackgroundImage})`,
+        backgroundAttachment: "fixed",
+        backgroundSize: "100% 100%",
+        width: '100%',
+        height: '100%',
+        paddingTop: 100,
+        paddingBottom: 100,
+      }}
+    >
+      <div className={board}>
 
-export default GamePlay
+      </div>
+    </div>
+  );
+};
+
+export default GamePlay;
--- src/Pages/GameStart/GameStart.js
+++ src/Pages/GameStart/GameStart.js
@@ -1,9 +1,23 @@
 import React from "react";
 import SideImage from "../../Assets/sideGrid.png";
-import Styles from './Styles.module.css';
+import Character from "../../Assets/character.png";
+import Styles from "./Styles.module.css";
 
-const GameStart = () => {
-  const {container, main} = Styles;
+const GameStart = (props) => {
+  
+  const {
+    main,
+    container,
+    char,
+    title,
+    middle,
+    text1,
+    text2,
+    gameGrid,
+    input,
+    button,
+    gridContainer,
+  } = Styles;
   return (
     <div className={main}>
       <div className={container}>
@@ -11,7 +25,29 @@ const GameStart = () => {
           <img src={SideImage} alt="Side grid" />
         </div>
 
-        <div></div>
+        <div className={middle}>
+          <img className={char} src={Character} alt="character" />
+          <div className={title}>{props.title}</div>
+
+          <div className={text1}>
+            The aim is to eat all the food in record time
+          </div>
+          <div className={text2}>Configure your game grid below 👇🏼</div>
+          <div
+            className={gridContainer}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <div className={gameGrid}>Game grid</div>
+
+            <input className={input} autoFocus min="5" max="12" type="number" />
+          </div>
+          <button onClick={() => {props.changeState()}} className={button}>
+            Start Game
+          </button>
+        </div>
 
         <div>
           <img src={SideImage} alt="Side grid" />
